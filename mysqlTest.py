bda3546cd2a1b2be41e81c13849a05df3fe89ab6
maosea0125 <maosea0125@163.com>
2017-03-06 16:58:17 +0800

    数据库类示例代码

--- mysqlTest.py
+++ mysqlTest.py
@@ -1,21 +1,31 @@
 #coding=utf-8
 import sys;
-import mysql.connector;
+from Mysqlib import Mysqlib;
 
-try:
-    conn = mysql.connector.connect(host='192.168.18.171', user='jmao', passwd='uy288a', db='wordpress', port='3308')
-    cursor = conn.cursor()
-except Exception:
-	sys.exit()
+config = {
+    'host': '192.168.18.171',
+    'port': '3308',
+    'user': 'jmao',
+    'passwd': '',
+    'db': 'wordpress',
+    'charset': 'utf8',
+    'timeout': 30
+}
+
+db = Mysqlib(config);
 
 #查询数据
-cursor.execute('SELECT * FROM wp_posts')
-for post in cursor:
+posts = db.fetchAll('SELECT * FROM wp_posts');
+for post in posts:
     print(post)
 
-cursor.execute('SELECT ID,post_title FROM wp_posts')
-for (ID, post_title) in cursor:
-    print(ID, post_title)
+post = db.fetchOne('SELECT * FROM wp_posts LIMIT 1');
+print(post);
 
-cursor.close();
-conn.close();
+db.query("INSERT INTO `wp_posts` VALUES (NULL, '1', '2017-02-16 22:25:22', '2017-02-16 14:25:22', '', '新生儿', '', 'inherit', 'open', 'closed', '', '%e6%96%b0%e7%94%9f%e5%84%bf', '', '', '2017-02-16 22:25:22', '2017-02-16 14:25:22', '', '194', 'http://doudou0911.com/wp-content/uploads/2017/02/新生儿.jpg', '0', 'attachment', 'image/jpeg', '0');")
+insertId = db.getInsertId();
+print(insertId);
+
+db.query('DELETE FROM wp_posts WHERE id = ' + str(insertId));
+
+db.close();
